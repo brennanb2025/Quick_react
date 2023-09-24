8f9e2f195c408898932b21beb293f76e28d66bd7
Brennan Benson <brennanbenson2025@u.northwestern.edu>
2023-09-24 11:42:02 -0500

    Change app title and icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+// DONE
+const firebaseConfig = {
+  apiKey: "AIzaSyD2NRWUeYzM5tZsdhM9SZQ9MkpPCRSf-Ao",
+  authDomain: "quick-react-baf2c.firebaseapp.com",
+  projectId: "quick-react-baf2c",
+  storageBucket: "quick-react-baf2c.appspot.com",
+  messagingSenderId: "757198641502",
+  appId: "1:757198641502:web:081d6fe17d1291ab3b17cf",
+  measurementId: "G-7BGP57ZHG3"
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
+const analytics = getAnalytics(App);
+
+/*const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -40,6 +58,6 @@ const App = () => {
       </header>
     </div>
   );
-};
+};*/
 
 export default App;
