aa16f49604635ebb15956dd2555936c79ff14e20
Brennan Benson <brennanbenson2025@u.northwestern.edu>
2023-09-27 16:53:59 -0500

    Fetch data

--- src/components/CourseDisplay.jsx
+++ src/components/CourseDisplay.jsx
@@ -1,13 +1,13 @@
 import './CourseDisplay.css';
 
 const CourseDisplay = ({course}) => (
-    <div className="card m-1 p-2 course-card">
-        <div className="card-body">
-            <h4 className="card-title">{course.term} CS {course.number}</h4>
-            <p className="card-text">{course.title}</p>
-            <div className="card-footer-custom">
+    <div className='card mb-4 p-2 course-card'>
+        <div className='card-body'>
+            <h4 className='card-title'>{course.term} CS {course.number}</h4>
+            <p className='card-text'>{course.title}</p>
+            <div className='card-footer-custom'>
                 <hr />
-                <p className="card-text pt-3 ps-4">{course.meets}</p>
+                <p className='card-text pt-3 ps-4'>{course.meets}</p>
             </div>
         </div>
     </div>
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,7 +2,7 @@ import CourseDisplay from './CourseDisplay';
 import './CourseList.css';
 
 const CourseList = ({courses}) => (
-    <div className="course-list">
+    <div className='course-list mt-4'>
         {
             Object.entries(courses).map(
                 ([id, course]) => <CourseDisplay key={id} course={course} />)
